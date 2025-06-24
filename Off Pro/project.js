2a8e386f0c6b3a3a042ebb3f24443d31f999a85a
Sasankdarsi <136076083+Sasankdarsi@users.noreply.github.com>
2025-06-24 22:52:19 +0530

    Update project.js

--- Off Pro/project.js
+++ Off Pro/project.js
@@ -1,4 +1,4 @@
-const API_KEY = "api_key=ae38fa09274bf55f518587023a99b4df";
+const API_KEY = "api_key="YOUR_API_KEY"";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&${API_KEY}`;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
